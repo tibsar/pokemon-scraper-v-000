f6f6df72e28d627113bc35e4f6b2bb7f790571ea
Sara Tibbetts <sxt4866@rit.edu>
2018-03-23 01:34:18 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -4,6 +4,7 @@ describe "Pokemon" do
   before do
     @db = SQLite3::Database.new(':memory:')
     @db.execute("DROP TABLE IF EXISTS pokemon")
+    binding.pry
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
   end
