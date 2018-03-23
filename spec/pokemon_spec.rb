140e1e9cbe3db842673385d89943602ca63fae02
Sara Tibbetts <sxt4866@rit.edu>
2018-03-23 01:32:48 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -43,6 +43,10 @@ describe "Pokemon" do
     let(:magikarp){Pokemon.find(129, @db)}
 
     before do
+      @db = SQLite3::Database.new(':memory:')
+      @db.execute("DROP TABLE IF EXISTS pokemon")
+      @sql_runner = SQLRunner.new(@db)
+      @sql_runner.execute_schema_migration_sql
       @sql_runner.execute_create_hp_column
     end
 
