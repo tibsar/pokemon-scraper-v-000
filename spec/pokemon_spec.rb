9d31dd99f7b178f56cdc8e96ec8de63cccac533d
Sara Tibbetts <sxt4866@rit.edu>
2018-03-23 01:25:58 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -46,14 +46,14 @@ describe "Pokemon" do
       @sql_runner.execute_create_hp_column
     end
 
-    it "knows that a pokemon have a default hp of 60" do
+    describe "knows that a pokemon have a default hp of 60" do
       it "Implement the bonus section of the README. Then remove this line."
       expect(@db.execute("knows_default_hp").flatten.first).to eq(60)
     end
 
     # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
     # He used splash. It wasn't very effective. It did one damage.
-    it "alters Pikachu's hp to 59" do
+    describe "alters Pikachu's hp to 59" do
       it "Implement the bonus section of the README. Then remove this line."
 
       pikachu.alter_hp(59)
@@ -61,7 +61,7 @@ describe "Pokemon" do
     end
 
     # Now we alter Magikarp's hp
-    it "alters Magikarp's hp" do
+    describe "alters Magikarp's hp" do
       it "Implement the bonus section of the README. Then remove this line."
 
       magikarp.alter_hp(0)
